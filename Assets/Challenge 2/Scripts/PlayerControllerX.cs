014899ff2c9affc834b520d3b5caf0dea61adffd
davidx12 <80353441+davidx12@users.noreply.github.com>
2024-09-10 17:34:00 +0200

    Update

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public Timer newTimer = new Timer();
+    public int measureTime = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        newTimer.Interval = 6;
+        newTimer.Elapsed += new ElapsedEventHandler(TimeStart);
+        newTimer.Enabled = true;
+        newTimer.Start();
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) & measureTime > 3)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            measureTime = 1;
+            newTimer.Stop();
+        }
+    }
+
+    private void TimeStart(object o, ElapsedEventArgs e)
+    {
+        measureTime++;
+    }
+}
